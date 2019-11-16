1bbb93734d611ec98c5f4b9fb8da55987dff537b
Samuel Grayson <sam@samgrayson.me>
2019-11-16 14:46:31 -0600

    use C++17 parallelism because it is better

--- src/run_sharing.cc
+++ src/run_sharing.cc
@@ -1,28 +1,10 @@
 #include <vector>
 #include <iostream>
-#include <functional>
 #include <assert.h>
-#include <omp.h>
+#include <algorithm>
+#include <execution>
 #include "DynamicLib.cc"
 #include "util.hh"
-
-const bool delay = true;
-
-// This is a dumb hack to randomize targets/test2.so
-// It uses the time for a seed,
-// so I want to start both trials at random times,
-// to verify that they get different results.
-// Getting different results in both threads,
-// indicates that the libraries really are loaded and initialized twice.
-#include <chrono>
-#include <thread>
-#include <random>
-void random_delay() {
-	std::random_device rd;
-	std::uniform_int_distribution<int> dist {0, 20};
-	std::this_thread::sleep_for(std::chrono::milliseconds(dist(rd) * 15));
-}
-
 
 int run_main(const std::vector<std::string>& args) {
 	DynamicLib lib (args[0]);
@@ -32,10 +14,11 @@ int run_main(const std::vector<std::string>& args) {
 
     char** argv = new char*[args.size()];
 	for (size_t i = 0; i < args.size(); ++i) {
-		argv[i] = new char[args[i].size()];
+		argv[i] = new char[args[i].size() + 1];
 
 		// this_main() could modify, so args[i].c_str won't work.
 		std::copy(args[i].cbegin(), args[i].cend(), argv[i]);
+		argv[i][args[i].size()] = '\0';
 	}
 
 	int ret = this_main(args.size(), argv);
@@ -48,37 +31,71 @@ int run_main(const std::vector<std::string>& args) {
 	return ret;
 }
 
+// shamelessly hoisted from
+// https://stackoverflow.com/a/53001490/1078199
+enum class Policy {seq, unseq, par_seq, par_unseq};
+template<class Iterator, class F>
+auto maybe_parallel_for_each(Policy policy, Iterator begin, Iterator end, F f) {
+	switch(policy) {
+	case Policy::par_unseq: return std::for_each(std::execution::par_unseq, begin, end, f);
+	case Policy::    unseq: return std::for_each(std::execution::    unseq, begin, end, f);
+	case Policy::      seq: return std::for_each(std::execution::      seq, begin, end, f);
+	case Policy::  par_seq: return std::for_each(std::execution::  par    , begin, end, f);
+  }
+}
+
 int main(int argc, char* const argv[]) {
 	std::string sep {";"};
 	std::vector<std::string> args {argv, argv + argc};
+
+	// parse flag arguments
+	Policy policy = Policy::par_unseq;
+	if (args[1] == "-u") {
+		policy = Policy::seq;
+		args.erase(args.begin() + 1);
+	}
+	if (args[1] == "-s") {
+		policy = Policy::par_seq;
+		args.erase(args.begin() + 1);
+	}
+	// no more flag arguments present in args
+
 	// args[0] was this program name
 	args[0] = sep;
 	// sep means "new sub arg list starts now"
 	std::vector<std::vector<std::string>> sub_args;
 	for (std::string arg : args) {
-		if (arg.compare(sep) == 0) {
+		if (arg == sep) {
 			sub_args.push_back(std::vector<std::string>{});
 		} else {
 			assert(sub_args.size() > 0);
+			// steal/move the arg
+			// I promise not to access "args" again
 			(sub_args.end() - 1)->push_back(std::move(arg));
 		}
 	}
 
-	std::vector<int> results(sub_args.size(), 0);
+	maybe_parallel_for_each(
+		policy,
+		sub_args.begin(),
+		sub_args.end(),
+		[](auto&& sub_arg) {
 
-	#pragma omp parallel for
-	for (std::size_t i = 0; i < sub_args.size(); ++i) {
-		if constexpr(delay) {
-			random_delay();
+			int ret = 0;
+			try {
+				ret = run_main(sub_arg);
+			} catch (const std::runtime_error& e) {
+				std::cout << "load ";
+				join(std::cout, sub_arg.begin(), sub_arg.end(), "\"", "\", \"", "\" ");
+				std::cout << "failed: " << e.what() << std::endl;
+			}
+
+			if (ret != 0) {
+				join(std::cout, sub_arg.begin(), sub_arg.end(), "\"", "\", \"", "\" ");
+				std::cout << "returned " << ret << std::endl;
+			}
 		}
-		results[i] = run_main(sub_args[i]);
-	}
-
-	for (std::size_t i = 0; i < sub_args.size(); ++i) {
-		std::cout << sub_args[i][0] << ":main ";
-		join(std::cout, sub_args[i].begin() + 1, sub_args[i].end(), "", " ", "");
-		std::cout << std::endl << "returned " << results[i] << std::endl;
-	}
+	);
 
 	return 0;
 }
